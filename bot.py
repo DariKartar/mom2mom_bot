32014f0a8200f53eb9f0d53ccea9a09295488255
DariKartar <darikartar@gmail.com>
2025-02-16 21:36:10 +0300

    Исправлена синтаксическая ошибка

--- bot.py
+++ bot.py
@@ -40,7 +40,7 @@ async def start_survey(message: types.Message, state: FSMContext):
     await state.finish()
     user_answers[message.from_user.id] = []
     await message.answer(
-        "Привет! Я — бот клуба «от Мамы к Маме (mom2mom)».
+        "Привет! Я — бот клуба «от Мамы к Маме (mom2mom)»."
 "
         "Спасибо за твой интерес! Сейчас я задам несколько вопросов."
     )
